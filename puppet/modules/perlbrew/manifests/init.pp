a13767730d6250bc913babd6cf1dd41839e35d26
William Belle <william.belle@gmail.com>
2016-05-29 22:00:55 +0200

    clean and update perl version

--- puppet/modules/perlbrew/manifests/init.pp
+++ puppet/modules/perlbrew/manifests/init.pp
@@ -1,7 +1,21 @@
+# Install Perlbrew
 class perlbrew {
+
+  # Perl version to install
+  $perlVersion = '5.8.9 5.10.1 5.12.5 5.14.4 5.16.3 5.18.4 5.20.3 5.22.2 5.24.0'
+
+  # Perlbrew root
+  $perlbrewRoot = 'PERLBREW_ROOT=/home/vagrant/perl5/perlbrew'
+
+  # Perlbrew path
+  $perlbrew = '/home/vagrant/perl5/perlbrew/bin/perlbrew'
+
+  # Add perlbrew to $PATH
+  $source = 'echo "source ~/perl5/perlbrew/etc/bashrc" >> /home/vagrant/.bashrc'
+
   exec { 'install perlbrew':
     require     => Package['curl'],
-    environment => 'PERLBREW_ROOT=/home/vagrant/perl5/perlbrew',
+    environment => $perlbrewRoot,
     command     => 'sudo curl -kL http://install.perlbrew.pl | bash',
     user        => 'vagrant',
     cwd         => '/home/vagrant',
@@ -11,22 +25,22 @@ class perlbrew {
 
   exec { 'add perlbrew to $PATH':
     unless    => 'grep -c "/perl5/perlbrew/etc/bashrc" /home/vagrant/.bashrc',
-    command   => 'echo "source ~/perl5/perlbrew/etc/bashrc" >> /home/vagrant/.bashrc',
+    command   => $source,
     user      => 'vagrant',
     logoutput => on_failure,
     before    => Exec['init perlbrew'],
   }
 
   exec { 'init perlbrew':
-    environment => 'PERLBREW_ROOT=/home/vagrant/perl5/perlbrew',
-    command     => '/home/vagrant/perl5/perlbrew/bin/perlbrew init',
+    environment => $perlbrewRoot,
+    command     => "${perlbrew} init",
     user        => 'vagrant',
     before      => Exec['install all perl'],
   }
 
   exec { 'install all perl':
-    environment => 'PERLBREW_ROOT=/home/vagrant/perl5/perlbrew',
-    command     => '/home/vagrant/perl5/perlbrew/bin/perlbrew install-multiple --notest 5.8.9 5.10.1 5.12.5 5.14.4 5.16.3 5.18.4 5.20.2 5.22.1',
+    environment => $perlbrewRoot,
+    command     => "${perlbrew} install-multiple --notest ${perlVersion}",
     timeout     => 10000,
     user        => 'vagrant',
     cwd         => '/home/vagrant',
@@ -35,16 +49,15 @@ class perlbrew {
   }
 
   exec { 'set default perl':
-    environment => 'PERLBREW_ROOT=/home/vagrant/perl5/perlbrew',
-    command     => '/home/vagrant/perl5/perlbrew/bin/perlbrew switch perl-5.20.2',
+    environment => $perlbrewRoot,
+    command     => "${perlbrew} switch perl-5.24.0",
     user        => 'vagrant',
-    cwd         => '/home/vagrant',
     before      => Exec['install cpanm'],
   }
 
   exec { 'install cpanm':
-    environment => 'PERLBREW_ROOT=/home/vagrant/perl5/perlbrew',
-    command     => '/home/vagrant/perl5/perlbrew/bin/perlbrew install-cpanm',
+    environment => $perlbrewRoot,
+    command     => "${perlbrew} install-cpanm",
     user        => 'vagrant',
     cwd         => '/home/vagrant',
   }
