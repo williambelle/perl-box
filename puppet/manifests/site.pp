03325ee5399760b748a38697f5bcc35bf3b2cef5
William Belle <william.belle@gmail.com>
2016-05-29 18:46:51 +0200

    fix init of box

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,13 +3,14 @@ node default {
     path => ['/bin', '/usr/bin/', '/sbin/', '/usr/sbin/', '/usr/local/bin/']
   }
 
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+  exec { 'apt-get -q -y update':
+    command => 'apt-get -q -y update'
   }
 
   $packages = [ 'build-essential', 'curl', 'libperl-critic-perl' ]
   package { $packages:
-    ensure => latest
+    ensure  => latest,
+    require => Exec['apt-get -q -y update']
   }
 
   include perlbrew
