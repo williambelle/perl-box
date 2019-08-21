2264d6819832d409932e2921230cd480db6f7843
William Belle <william.belle@gmail.com>
2019-08-21 19:34:17 +0200

    update perl versions

--- bin/perlbrew.sh
+++ bin/perlbrew.sh
@@ -3,7 +3,7 @@
 PERLBREW_BIN='/home/vagrant/perl5/perlbrew/bin/perlbrew'
 
 PERL_OLD='5.8.9 5.10.1 5.12.5 5.14.4 5.16.3 5.18.4 '
-PERL_NEW='5.20.3 5.22.4 5.24.4 5.26.2 5.28.0'
+PERL_NEW='5.20.3 5.22.4 5.24.4 5.26.3 5.28.2 5.30.0'
 
 # Install Perlbrew
 sudo curl -kL https://install.perlbrew.pl | bash
@@ -22,4 +22,4 @@ $PERLBREW_BIN exec cpanm Test::CheckManifest Test::Pod::Coverage
 $PERLBREW_BIN exec cpanm Test::Pod Test::Perl::Critic
 
 # Switch to latest Perl version
-$PERLBREW_BIN switch perl-5.28.0
+$PERLBREW_BIN switch perl-5.30.0
